8595ee5cdb4a8df6abe2e24508cad7351548a91a
Will Huxtable <w@zif.io>
2017-03-21 12:44:47 +0000

    Copyright notice

--- cmd/zifd/main.go
+++ cmd/zifd/main.go
@@ -1,3 +1,20 @@
+/*
+ *  Zif
+ *  Copyright (C) 2017 Zif LTD
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package main
 
 import (
@@ -11,6 +28,7 @@ import (
 	"github.com/spf13/viper"
 	zif "github.com/zif/zif"
 	data "github.com/zif/zif/data"
+	dht "github.com/zif/zif/dht"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -125,6 +143,14 @@ func main() {
 	if err != nil {
 		log.Error(err.Error())
 	}
+
+	addr1 := dht.Address{
+		Raw: []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+
+	addr1str, _ := addr1.String()
+
+	fmt.Println(addr1str)
 
 	// Listen for SIGINT
 	sigchan := make(chan os.Signal, 1)
